e840d33bf3035eecae46da8c50fc4b56a618e8e0
Katie Wright <katie.wrght@gmail.com>
2019-11-14 12:49:08 +0100

    create initial TODO components

---
+++ components/ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ToDoItem from './ToDoItem';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default ToDo;
